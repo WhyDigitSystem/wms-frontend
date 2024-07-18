103cf7cd99e4c6fdf9fd864591b89839c6f2a8c7
Karupu-whydigit <karupu@whydigit.in>
2024-07-18 18:17:22 +0530

    Action component & ToastComponent created done

--- src/views/pages/basic-masters/CountryMaster.js
+++ src/views/pages/basic-masters/CountryMaster.js
@@ -20,10 +20,14 @@ import EditIcon from '@mui/icons-material/Edit';
 import { showErrorToast, showSuccessToast } from 'utils/toastUtils';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import ActionButton from 'utils/ActionButton';
+import ToastComponent, { showToast } from 'utils/toast-component';
 
 export const CountryMaster = () => {
   const [orgId, setOrgId] = useState(1000000001);
   const [loginUserName, setLoginUserName] = useState('Karupu');
+  const [isLoading, setIsLoading] = useState(false);
+
   const [formData, setFormData] = useState({
     active: true,
     countryCode: '',
@@ -130,6 +134,7 @@ export const CountryMaster = () => {
     }
 
     if (Object.keys(errors).length === 0) {
+      setIsLoading(true);
       const saveFormData = {
         ...(editId && { id: editId }),
         active: formData.active,
@@ -143,19 +148,26 @@ export const CountryMaster = () => {
 
       axios
         .post(`${process.env.REACT_APP_API_URL}/api/commonmaster/createUpdateCountry`, saveFormData)
+
         .then((response) => {
-          if (response.data.statusFlag === 'Error') {
+          if (response.data.status === true) {
             console.log('Response:', response.data);
-            showErrorToast(response.data.paramObjectsMap.errorMessage);
-          } else {
-            showSuccessToast(response.data.paramObjectsMap.message);
+
+            showToast('success', editId ? ' Country Updated Successfully' : 'Country created successfully');
+
             handleClear();
             getAllCountries();
+            setIsLoading(false);
+          } else {
+            showToast('error', response.data.paramObjectsMap.errorMessage || 'Country creation failed');
+            setIsLoading(false);
           }
         })
         .catch((error) => {
           console.error('Error:', error);
-          showErrorToast('An error occurred while saving the country');
+          showToast('error', 'Country creation failed');
+
+          setIsLoading(false);
         });
     } else {
       setFieldErrors(errors);
@@ -182,107 +194,19 @@ export const CountryMaster = () => {
   };
   return (
     <>
-      <div>{/* <ToastContainer /> */}</div>
       <div className="card w-full p-6 bg-base-100 shadow-xl" style={{ padding: '20px', borderRadius: '10px' }}>
         <div className="row d-flex ml">
           <div className="d-flex flex-wrap justify-content-start mb-4" style={{ marginBottom: '20px' }}>
-            <Tooltip title="Search" placement="top">
-              <ButtonBase sx={{ borderRadius: '12px', marginRight: '10px' }}>
-                <Avatar
-                  variant="rounded"
-                  sx={{
-                    ...theme.typography.commonAvatar,
-                    ...theme.typography.mediumAvatar,
-                    transition: 'all .2s ease-in-out',
-                    background: theme.palette.secondary.light,
-                    color: theme.palette.secondary.dark,
-                    '&[aria-controls="menu-list-grow"],&:hover': {
-                      background: theme.palette.secondary.dark,
-                      color: theme.palette.secondary.light
-                    }
-                  }}
-                  ref={anchorRef}
-                  aria-haspopup="true"
-                  color="inherit"
-                >
-                  <SearchIcon size="1.3rem" stroke={1.5} />
-                </Avatar>
-              </ButtonBase>
-            </Tooltip>
-
-            <Tooltip title="Clear" placement="top">
-              {' '}
-              <ButtonBase sx={{ borderRadius: '12px', marginRight: '10px' }} onClick={handleClear}>
-                <Avatar
-                  variant="rounded"
-                  sx={{
-                    ...theme.typography.commonAvatar,
-                    ...theme.typography.mediumAvatar,
-                    transition: 'all .2s ease-in-out',
-                    background: theme.palette.secondary.light,
-                    color: theme.palette.secondary.dark,
-                    '&[aria-controls="menu-list-grow"],&:hover': {
-                      background: theme.palette.secondary.dark,
-                      color: theme.palette.secondary.light
-                    }
-                  }}
-                  ref={anchorRef}
-                  aria-haspopup="true"
-                  color="inherit"
-                >
-                  <ClearIcon size="1.3rem" stroke={1.5} />
-                </Avatar>
-              </ButtonBase>
-            </Tooltip>
-
-            <Tooltip title="List View" placement="top">
-              {' '}
-              <ButtonBase sx={{ borderRadius: '12px' }} onClick={handleView}>
-                <Avatar
-                  variant="rounded"
-                  sx={{
-                    ...theme.typography.commonAvatar,
-                    ...theme.typography.mediumAvatar,
-                    transition: 'all .2s ease-in-out',
-                    background: theme.palette.secondary.light,
-                    color: theme.palette.secondary.dark,
-                    '&[aria-controls="menu-list-grow"],&:hover': {
-                      background: theme.palette.secondary.dark,
-                      color: theme.palette.secondary.light
-                    }
-                  }}
-                  ref={anchorRef}
-                  aria-haspopup="true"
-                  color="inherit"
-                >
-                  <FormatListBulletedTwoToneIcon size="1.3rem" stroke={1.5} />
-                </Avatar>
-              </ButtonBase>
-            </Tooltip>
-            <Tooltip title="Save" placement="top">
-              {' '}
-              <ButtonBase sx={{ borderRadius: '12px', marginLeft: '10px' }} onClick={handleSave}>
-                <Avatar
-                  variant="rounded"
-                  sx={{
-                    ...theme.typography.commonAvatar,
-                    ...theme.typography.mediumAvatar,
-                    transition: 'all .2s ease-in-out',
-                    background: theme.palette.secondary.light,
-                    color: theme.palette.secondary.dark,
-                    '&[aria-controls="menu-list-grow"],&:hover': {
-                      background: theme.palette.secondary.dark,
-                      color: theme.palette.secondary.light
-                    }
-                  }}
-                  ref={anchorRef}
-                  aria-haspopup="true"
-                  color="inherit"
-                >
-                  <SaveIcon size="1.3rem" stroke={1.5} />
-                </Avatar>
-              </ButtonBase>
-            </Tooltip>
+            <ActionButton title="Search" icon={SearchIcon} onClick={() => console.log('Search Clicked')} />
+            <ActionButton title="Clear" icon={ClearIcon} onClick={handleClear} />
+            <ActionButton title="List View" icon={FormatListBulletedTwoToneIcon} onClick={handleView} />
+            <ActionButton
+              title="Save"
+              icon={SaveIcon}
+              isLoading={isLoading}
+              onClick={() => handleSave()}
+              margin="0 10px 0 10px"
+            /> &nbsp;{' '}
           </div>
         </div>
         {listView ? (
@@ -333,6 +257,9 @@ export const CountryMaster = () => {
             </div>
           </>
         )}
+      </div>
+      <div>
+        <ToastComponent />
       </div>
     </>
   );
