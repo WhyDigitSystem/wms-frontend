a4be77e658fb9d1b8a2b9865952a226371651149
Karupu-whydigit <karupu@whydigit.in>
2024-09-12 11:43:50 +0530

    report component created

--- src/views/pages/reports/ReportMain.js
+++ src/views/pages/reports/ReportMain.js
@@ -4,6 +4,9 @@ import Tabs from '@mui/material/Tabs';
 import React from 'react';
 import StockConsolidation from './StockConsolidation';
 import StockConsolidationBinWise from './StockConsolidationBinWise';
+import StockLedgerReport from './StockLedgerReport';
+import { StockBatchWiseReport } from './StockBatchWiseReport';
+import StockBinBatchStatusWiseReport from './StockBinBatchStatusWiseReport';
 
 const ReportMain = () => {
   const [value, setValue] = React.useState(0);
@@ -18,10 +21,18 @@ const ReportMain = () => {
           <Tabs value={value} onChange={handleChange} textColor="secondary" indicatorColor="secondary" aria-label="secondary tabs example">
             <Tab value={0} label="Stock Consolidation" />
             <Tab value={1} label="Stock Consolidation BinWise" />
+            <Tab value={2} label="Stock Ledger Report" />
+            <Tab value={3} label="Stock Batch Wise Report" />
+            <Tab value={4} label="Stock Bin Batch Status Wise Report" />
           </Tabs>
         </Box>
-        <Box sx={{ padding: 2 }}>{value === 0 && <StockConsolidation />}</Box>
-        <Box sx={{ padding: 2 }}>{value === 1 && <StockConsolidationBinWise />}</Box>
+        <Box sx={{ padding: 2 }}>
+          {value === 0 && <StockConsolidation />}
+          {value === 1 && <StockConsolidationBinWise />}
+          {value === 2 && <StockLedgerReport />}
+          {value === 3 && <StockBatchWiseReport />}
+          {value === 4 && <StockBinBatchStatusWiseReport />}
+        </Box>
       </div>
     </>
   );
---
+++ src/views/pages/reports/StockBatchWiseReport.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const StockBatchWiseReport = () => {
+  return <div>StockBatchWiseReport</div>;
+};
